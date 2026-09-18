30bb1d9758dd0eb3818ae3c59c768ff4aad121a4
Lisandro Braun <lisandrobk@icloud.com>
2026-09-18 12:11:24 -0600

    Implement player controls, shooting, collisions, and asteroid splitting

---
+++ asteroid.py
@@ -0,0 +1,33 @@
+import random
+
+import pygame
+
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt: float) -> None:
+        self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        first_velocity = self.velocity.rotate(angle)
+        second_velocity = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first = Asteroid(self.position.x, self.position.y, new_radius)
+        second = Asteroid(self.position.x, self.position.y, new_radius)
+        first.velocity = first_velocity * 1.2
+        second.velocity = second_velocity * 1.2
